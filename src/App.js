df5b93dbba8101de9df964738efa907e360bce12
Kim <rodrigkim@gmail.com>
2022-10-03 11:57:08 -0400

    second attempt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+import Upload from './Upload.js';
+import About from './About.js';
+import Home from './Home';
+import NavBar from './NavBar';
+import data from './db.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home data="data"/>
+        </Route>
+        <Route path="/upload">
+          <Upload />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/Upload.js
@@ -0,0 +1,17 @@
+function Upload (){
+
+    return (
+        <div>
+            <h1>Upload here</h1>
+            <form>
+                <input type="text" placeholder="Listing name..."/>
+                <input type="text" placeholder="Description..."/>
+                <input type="text" placeholder="Price..."/>
+                <input type="text" placeholder="Neighborhood..."/>
+                <input type="text" placeholder="Contact Email..."/>
+            </form>
+        </div>
+    )
+}
+
+export default Upload
